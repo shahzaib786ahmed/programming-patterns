246616c7df2d41e84246a149bf375b7051d854d7
shahzaib786ahmed <ahmedshahzaib203@gmail.com>
2024-12-09 01:06:47 -0500

    Final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,7 @@
 package org.example;
-
-import org.example.controller.*;
-import org.example.model.*;
 import org.example.view.WelcomeView;
 
 import javax.swing.*;
-import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
